8cb4e17961f60f62066218e24e77cdf88d5cc1d6
Sherwood, Bill <sherwood_bill@yahoo.com>
2021-10-21 16:11:59 -0400

    a noise change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 import Cart from './components/Cart/Cart';
 
-
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
